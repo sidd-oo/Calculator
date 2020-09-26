b1b587446fb3aba6734795415f266f320cdcca13
SIDDHARTH SAHOO <sahoosiddharth3@gmail.com>
2020-09-26 23:52:08 +0530

    operate function written

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,40 @@
 let operandOne = 0;
 let operandTwo = 0;
+let operator = '';
+let result;
 let tempResult = 0;
-let operator ;
 
 
-function operate(operator, operandOne, operandTwo){
-    if(operator == 'add'){
-        return operandOne + operandTwo;
-    }else if(operator == 'subtract'){
-        return operandOne - operandTwo;
-    }else if(operator == 'multiply'){
-        return operandOne * operandTwo;
-    }else if(operator == 'divide'){
-        if(operatorTwo != 0){
-            return operandOne / operandTwo;
-        }else{
-            return "ERROR : Division not possible"
-        }
+function add(operandOne, operandTwo){
+    return operandOne + operandTwo;
+}
+
+function subtract(operandOne, operandTwo){
+    return operandOne - operandTwo;
+}
+
+function multiply(operandOne, operandTwo){
+    return operandOne * operandTwo;
+}
+
+function divide(operandOne, operandTwo){
+    if(operatorTwo != 0){
+        return operandOne / operandTwo;
+    }else{
+        return "ERROR : Division not possible"
     }
 }
 
+function operate(operator, operandOne, operandTwo){
+    if(operator == 'add'){
+        result = add(operandOne, operandTwo);
+    }else if(operator == 'subtract'){
+        result = subtract(operandOne, operandTwo);
+    }else if(operator == 'multiply'){
+        result = multiply(operandOne, operandTwo);
+    }else if(operator == 'divide'){
+        result = divide(operandOne, operandTwo);
+    }
+}
 
 
